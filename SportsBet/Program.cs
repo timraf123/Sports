d5471191f327e12c65aadc56846c9abc54d38e3c
timraf123 <74038553+timraf123@users.noreply.github.com>
2023-02-25 12:39:24 +1100

    Add files via upload

--- SportsBet/Program.cs
+++ SportsBet/Program.cs
@@ -12,9 +12,21 @@ namespace SportsBet
     {
         static void Main(string[] args)
         {
+            // AAdding all the NFL teams?
+    
+            List<Team> Teams = new List<Team>();
+            // Add more sports
 
-            Team team = new Team("Tampa Bay Buccaneers");
-            team.Test();   
+            //Team team = new Team("Tampa Bay Buccaneers");
+            Teams.Add(new Team("Tampa Bay Buccaneers"));
+            Teams.Add(new Team("Chicago Bears"));
+            Teams.Add(new Team("New York Giants"));
+
+            foreach (Team team in Teams)
+            {
+                Console.WriteLine(team.teamname);
+                team.Test();
+            }
         }
     }
 }
